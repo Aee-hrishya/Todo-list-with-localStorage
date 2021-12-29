c60e20de8180d9034d8c5af6b07bb53b4e129ac3
hrishikesh <hshinde166@gmail.com>
2021-12-29 23:55:58 +0530

    Main part of the application where we connect all the components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import ContextItem from './Components/Context/ContextItem';
 import './App.css';
+import TodoForm from './Components/TodoForm';
+
 
 function App() {
+
+  const [todos, setTodos] = useState([]); //Initial state of the application
+
+  //We use the useEffect hook to get the items from the localstorage if any and we want run it once at the start of the application
+  useEffect(() => {
+    const localTodos = localStorage.getItem("todos");
+
+    if(localTodos){
+      setTodos(JSON.parse(localTodos));
+    }
+  }, [])
+
+  //The function to add task to the list
+  const addTodos = async (todo) => {
+
+    setTodos([...todos, todo]);
+
+  };
+
+  //We use useEffect once again to set items to localstorage every time there is a change in todos
+  useEffect(() => {
+
+    localStorage.setItem("todos", JSON.stringify(todos));
+
+  }, [todos])
+
+  //function to remove the task from the list
+  const removeTodo = (id) => {
+
+    setTodos(todos.filter(todo => todo.id !== id));
+
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <ContextItem.Provider value={{todos, addTodos, removeTodo}}> {/*Our provider is over here */}
+      <TodoForm />
+    </ContextItem.Provider>
   );
 }
 
