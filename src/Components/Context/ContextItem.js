1e71f140096fc934fa58e201fe208736c50c7da9
hrishikesh <hshinde166@gmail.com>
2021-12-29 23:56:22 +0530

    We created the context here to handle the states at a central place

---
+++ src/Components/Context/ContextItem.js
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+const ContextItem = createContext();//Creating a context with which we want to use to manage all the states at one place
+
+export default ContextItem;
